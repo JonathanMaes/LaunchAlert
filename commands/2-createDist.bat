340a0ccaead09ace0211662dfa447ceda8e0b4cd
Jonathan Maes <jonathan145b@hotmail.com>
2019-09-04 14:14:59 +0200

    Release v1.0.1

--- commands/2-createDist.bat
+++ commands/2-createDist.bat
@@ -1,6 +1,6 @@
 @echo off
 cd ../source
-pyinstaller main.spec --upx-dir=..\upx-3.95-win64
+pyinstaller main.spec --onedir
 echo d | XCOPY /C /Y /K /S /Q "dist/main" "../dist"
 rmdir dist /S /Q
 rmdir build /S /Q
